a3af79d983d3124cfa1c81cf5da2c70b3f31b6a0
Kristina <Julia@DESKTOP-V1SKVB5>
2024-02-12 16:44:43 +0200

    Juhend 2 Ülesanne 2 lüliti

---
+++ Juhen2Lüliti/USwitch.Designer.vb
@@ -0,0 +1,61 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class USwitch
+    Inherits System.Windows.Forms.UserControl
+
+    'UserControl1 overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(USwitch))
+        Me.pbSwitch = New System.Windows.Forms.PictureBox()
+        Me.imLoend = New System.Windows.Forms.ImageList(Me.components)
+        CType(Me.pbSwitch, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'pbSwitch
+        '
+        Me.pbSwitch.Location = New System.Drawing.Point(380, 330)
+        Me.pbSwitch.Name = "pbSwitch"
+        Me.pbSwitch.Size = New System.Drawing.Size(209, 48)
+        Me.pbSwitch.TabIndex = 0
+        Me.pbSwitch.TabStop = False
+        '
+        'imLoend
+        '
+        Me.imLoend.ImageStream = CType(resources.GetObject("imLoend.ImageStream"), System.Windows.Forms.ImageListStreamer)
+        Me.imLoend.TransparentColor = System.Drawing.Color.Transparent
+        Me.imLoend.Images.SetKeyName(0, "switch off.jpg")
+        Me.imLoend.Images.SetKeyName(1, "switch on.jpg")
+        '
+        'USwitch
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Controls.Add(Me.pbSwitch)
+        Me.Name = "USwitch"
+        Me.Size = New System.Drawing.Size(800, 450)
+        CType(Me.pbSwitch, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents pbSwitch As PictureBox
+    Friend WithEvents imLoend As ImageList
+End Class
---
+++ Juhen2Lüliti/USwitch.vb
@@ -0,0 +1,18 @@
+﻿Public Class USwitch
+    ' Klassisisene meetod pildi määramiseks
+    Private Sub selectSwitch(ByVal intSwitch As Integer)
+        pbSwitch.Image = imLoend.Images(intSwitch)
+    End Sub
+
+    ' Avalik liidesmeetod
+    Public Sub setSwitch(ByVal strSwitch As String)
+        Select Case strSwitch
+            Case "Off"
+                Me.selectSwitch(0)
+            Case "On"
+                Me.selectSwitch(1)
+        End Select
+
+    End Sub
+
+End Class
